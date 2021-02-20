30247042fff765a8e69bfe1e857d9244b81ff850
Rabidus4k <38137224+Rabidus4k@users.noreply.github.com>
2021-02-20 03:39:11 +0300

    first

---
+++ Assets/TimeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    private void Start()
+    {
+        Time.timeScale = 1;
+    }
+    public void SetTimeScale(float timeScale)
+    {
+        Time.timeScale = timeScale;
+    }
+}
